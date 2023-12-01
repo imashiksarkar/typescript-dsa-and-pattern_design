ab34da99b4d371c7bc5412c4ad9314a2d2a754b8
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2023-12-01 21:33:33 +0600

    - add areThereDuplicates

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import { sameFrequency } from "./lib"
+import { areThereDuplicates, sameFrequency } from "./lib"
 
 // console.log(validAnagram("anagram", "nagaram"))
 // console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]))
 // console.log(countUniqueValues([1, 1, 1, 1, 1, 2]))
 // console.log(countUniqueValues([-2, -1, -1, 0, 1]))
+// console.log(sameFrequency(556892, 556829))
 
-console.log(sameFrequency(556892, 556829))
+console.log(areThereDuplicates(1, 2, 3, 5, 7, 1))
---
+++ lib/areThereDuplicates.ts
@@ -0,0 +1,10 @@
+export default <T extends number | string>(...args: T[]) => {
+  const numObj: Record<any, any> = {}
+
+  for (const char of args) {
+    numObj[char] = numObj[char] ? numObj[char] + 1 : 1
+    if (numObj[char] > 1) return true
+  }
+
+  return false
+}
