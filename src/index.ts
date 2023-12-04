aa9675cbbbcce8534db4583bd5366fdcaf52f9ce
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2023-12-05 01:32:42 +0600

    - add dist to .gitignore

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,42 @@ import {
   averagePair,
   isSubsequence,
   sameFrequency,
-} from "./lib"
+} from './lib'
 
+const main = () => {
+  console.log(isSubsequence('hello', 'hello world')) // true
+  console.log(isSubsequence('sing', 'sting')) // true
+  console.log(isSubsequence('abc', 'abracadabra')) // true
+  console.log(isSubsequence('abc', 'acb')) // false
+}
 
-console.log(isSubsequence("hello", "hello world")) // true
-console.log(isSubsequence("sing", "sting")) // true
-console.log(isSubsequence("abc", "abracadabra")) // true
-console.log(isSubsequence("abc", "acb")) // false
+// main()
+
+// recursion
+
+const sum = (result: number, num: number): number => {
+  if (num < 0) return 0
+  // if (num > 0) {
+  --num
+  //   sum(result + num, num)
+  //   console.log(result, num)
+  // }
+
+  return sum(result + num, num)
+  // return 0
+  // let result = 0
+}
+
+// console.log(sum(0, 4))
+
+const sumRecursive = (n: number): number => {
+  // Base case: if n is 1, return 1
+  if (n === 1) return 1
+
+  // Recursive case: sum of numbers from 1 to n is n + sum of numbers from 1 to (n-1)
+  return n + sumRecursive(n - 1)
+}
+
+// Calculate the sum of numbers from 1 to 4
+const result = sumRecursive(4)
+console.log(result) // Output: 10
